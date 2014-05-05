e01b91ca22e2f0633dc8133079e5c85bcbde710a
bibig <liushuxun@gmail.com>
2014-05-05 14:09:36 +0800

    kick off

---
+++ index.js
@@ -0,0 +1,197 @@
+exports.isEmpty    = isEmpty;
+exports.trim       = trim;
+exports.filter     = filter;
+exports.merge      = merge;
+exports.mergeArray = mergeArray;
+exports.clone      = clone;
+exports.cloneArray = cloneArray;
+exports.humanSize  = humanSize;
+
+
+
+function trim (v) {
+  
+  if ( typeof v == 'string') {
+      return v.replace(/^\s\s*/, '').replace(/\s\s*$/, '');
+  } else {
+      return v;
+  }
+
+}
+
+function isEmpty (v) {
+
+  if (Array.isArray(v)) {
+    return v.length === 0;  
+  }
+
+  if (v === undefined || v === null || v === '') {
+    return true;
+  }
+
+  if (typeof v === 'object') {
+    return Object.keys(v).length === 0 ;
+  }
+
+  return false;
+}
+
+/**
+ * merge keys of the default object info the target object
+ * ...
+ * @author bibig@me.com
+ * @update [2014-05-05 11:50:11]
+ * @param  {object} targetObj
+ * @param  {object} defaultObj
+ * @return {object}
+ *
+ * @usage:  
+ *    a = merge(a, b);
+ *    do not use like this: 
+ *    merge(a, b) // unless you sure a is an object.
+ *    when a is null or undefined, a should not be given values. merge will return a new object.
+ * 
+ */
+function merge (targetObj, defaultObj) {
+
+  if (! targetObj) { targetObj = {};}
+
+  if (! defaultObj || typeof defaultObj !== 'object') { return targetObj; }
+
+  if (typeof targetObj !== 'object') { targetObj = {}; }
+  
+  Object.keys(defaultObj).forEach(function (key) {
+
+    if (targetObj[key] === undefined) {
+      targetObj[key] = defaultObj[key];
+    }
+
+  });
+
+  return targetObj;            
+}
+
+/**
+ * filter target object by given keys
+ * the difference between `filter` and `clone` is that filter copyed by referecne, but clone duplicated
+ * @author bibig@me.com
+ * @update [2014-05-05 11:52:26]
+ * @param  {object} target
+ * @param  {array or string} keys
+ * @return {object}
+ * @usage:
+ *   filter(target, ['k1', 'k2']);
+ *   filter(target, 'k1, k2');
+ */
+function filter (target, keys) {
+  var safe = {};
+  
+  if (typeof keys === 'string') {
+    keys = keys.split(',');
+  }
+
+  if ( ! keys || ! Array.isArray(keys) ) {
+    return safe;
+  }
+
+  keys.forEach(function (key) {
+    key = trim(key);
+
+    if (target[key] !== undefined && target[key] !== null && target[key] !== '') {
+      safe[key] = target[key];
+    }
+
+  });
+  
+  return safe;
+}
+
+function cloneArray (target) {
+  var copycat = [];
+
+  target.forEach(function (ele) {
+
+    if (Array.isArray(ele)) {
+      copycat.push(cloneArray(ele));
+    } else if (typeof ele == 'object') {
+      copycat.push(clone(ele));
+    } else {
+      copycat.push(ele);
+    }
+
+  });
+
+  return copycat;
+}
+
+/**
+ * clone target object.
+ * ...
+ * @author bibig@me.com
+ * @update [2014-05-05 11:50:05]
+ * @param  {object} target
+ * @param  {array or string} keys
+ * @return {object}
+ * @usage: 
+ *  clone(target, ['k1', 'k2']);
+ *  clone(target, 'k1, k2');
+ */
+function clone (target, keys) {
+  var copycat;
+  
+  if (target === null || target === undefined || target === '') {
+    return target;
+  }
+
+  if (Array.isArray(target)) {
+    return cloneArray(target);
+  } 
+    
+  if (typeof target === 'object') {
+    copycat = {};
+
+    if ( typeof keys === 'string') {
+      keys = keys.split(',');
+    }
+
+    if ( ! keys && ! Array.isArray(keys) ) {
+      keys = Object.keys(target);  
+    }
+
+    if ( keys.length > 0 ) {
+      keys.forEach(function (key) {
+        key = trim(key);
+        copycat[key] = clone(target[key]);
+      });
+    }
+
+    return copycat;
+  }
+
+  return target;
+}
+
+function mergeArray (a, b) {
+  var c = [].concat(a);
+  
+  b.forEach(function (key) {
+    
+    if (c.indexOf(key) == -1) {
+      c.push(key);
+    }
+
+  });
+  
+  return c;
+}
+
+function humanSize (size) {
+  
+  if (size < 1048576) { // 1m
+    return parseInt(size * 10 / 1024) / 10  + 'k';
+  } else {
+    return parseInt(size * 10 / 1048576 ) / 10  + 'm';
+  }
+
+}
+
---
+++ test/basic.js
@@ -0,0 +1,235 @@
+// utils.js unit test
+
+var utils = require('../index');
+var should = require('should');
+
+describe('utils unit test', function () {
+
+  describe('trim unit test', function () {
+    it('test trim', function () {
+      utils.trim(' a ').should.be.exactly('a');
+      utils.trim(' a b ').should.be.exactly('a b');
+    });  
+  });
+
+  describe('isEmpty unit test', function () {
+    (utils.isEmpty(null)).should.ok;
+    (utils.isEmpty('')).should.ok;
+    (utils.isEmpty(undefined)).should.ok;
+
+    (utils.isEmpty([])).should.ok;
+    (utils.isEmpty({})).should.ok;
+
+    (utils.isEmpty('a')).should.not.be.ok;
+    (utils.isEmpty(1)).should.not.be.ok;
+    (utils.isEmpty(0)).should.not.be.ok;
+
+    (utils.isEmpty([null])).should.not.be.ok;
+    (utils.isEmpty([''])).should.not.be.ok;
+    (utils.isEmpty([undefined])).should.not.be.ok;
+    (utils.isEmpty({a: null})).should.not.be.ok;    
+  });
+
+  describe('clone unit test', function () {
+    var mixedHash = {
+      'k1': 'v1',
+      'k2': [1, 2, 3, 4],
+      'k3': {
+        'sub_k1': 'sub_v1',
+        'sub_k2': 'sub_v2',
+        'sub_k3': ['a', 'b', 'c', 'd', 'e', {
+          'sub_sub_k1': 'sub_sub_v1',
+          'sub_sub_k2': 'sub_sub_v2'
+        }]
+      }
+    };
+
+    it('basic hash clone', function () {
+      var a = {'k1': 'v1', 'k2': 'v2', 'k3': 'v3'};
+      var b = utils.clone(a, ['k1', 'k2']);
+      var c = utils.clone(a, 'k1, k3');
+
+      b.should.have.property('k1');
+      b.should.have.property('k2');
+
+      c.should.have.property('k1');
+      c.should.have.property('k3');
+
+    });
+
+    it('clone has without keys defined', function () {
+      var a = {'k1': 'v1', 'k2': 'v2', 'k3': 'v3'};
+      var b = utils.clone(a);
+
+      b.should.eql(a);
+
+      b.k1 = null;
+      (a.k1).should.not.be.exactly(null);
+    });
+
+    it('clone mixed object', function () {
+      var b = utils.clone(mixedHash);
+      var c = utils.clone(mixedHash, ['k3']);
+
+      b.should.have.property('k1');
+      b.should.have.property('k2');
+      b.should.have.property('k3');
+
+      b.k2.should.eql([1, 2, 3, 4]);
+      b.k2.should.not.equal(mixedHash.k2);
+      
+      b.k3.should.have.property('sub_k1');
+      b.k3.should.have.property('sub_k2');
+      b.k3.should.have.property('sub_k3');
+      
+      (b.k3.sub_k3[0]).should.equal('a');
+      (b.k3.sub_k3[5]).should.have.property('sub_sub_k1');
+
+      c.should.have.property('k3');
+      c.k3.should.have.property('sub_k1');
+      c.k3.should.have.property('sub_k2');
+      c.k3.should.have.property('sub_k3');
+      
+      (c.k3.sub_k3[0]).should.equal('a');
+      (c.k3.sub_k3[5]).should.have.property('sub_sub_k1');
+
+    });
+
+    it('clone array', function () {
+      var a = utils.clone(mixedHash.k2);
+
+      (Array.isArray(a)).should.equal(true);
+      a.should.eql(mixedHash.k2);
+      a.should.not.exactly(mixedHash.k2);
+    });
+
+    it('clone normal value', function () {
+      var v = 'v1';
+      var a = utils.clone(v);
+      a.should.exactly(v);
+    });
+
+    it('clone undefined value', function () {
+      var un = undefined;
+      var a = utils.clone(un);
+      (a === undefined).should.ok;
+    });
+
+    it('clone empty hash', function () {
+      var empty = {};
+      var a = utils.clone(empty);
+
+      a.should.eql({});
+    });
+
+    it('clone empty array', function () {
+      var empty = [];
+      var a = utils.clone(empty);
+
+      a.should.eql([]);
+    });
+
+    it('clone empty value', function () {
+      var empty = '';
+      var a = utils.clone(empty);
+
+      a.should.exactly('');
+    });
+
+    it('clone null', function () {
+      var empty = null;
+      var a = utils.clone(empty);
+
+      (a === null).should.ok;
+    });
+
+  });
+
+  describe('merge unit test', function () {
+
+    it('basic test', function () {
+      var a = {
+        k1: 'a1',
+        k2: 'a2'
+      };
+      var b = {
+        k1: 'b1',
+        k2: 'b2',
+        k3: 'b3',
+        k4:  null
+      };
+      var c = utils.merge(a, b);
+      
+      c.should.exactly(a);
+      c.should.have.property('k3');
+      c.k1.should.exactly('a1');
+      c.k2.should.exactly('a2');
+      c.k3.should.exactly('b3');
+      (c.k4 === null).should.ok;  
+    });
+
+    it('test merge object into empty object', function () {
+      var a = null;
+      var b = {
+        k1: 'b1',
+        k2: 'b2',
+        k3: 'b3',
+        k4:  null
+      };
+      a = utils.merge(a, b);
+      a.should.eql(b);
+
+      a = '';
+      a = utils.merge(a, b);
+      a.should.eql(b);
+
+      a = undefined;
+      a = utils.merge(a, b);
+      a.should.eql(b);
+
+    });
+
+    it('test merge empty into object', function () {
+      var a = {
+        k1: 'a1',
+        k2: 'a2',
+        k3: 'a3'
+      };
+      var c = utils.merge(a, null);
+      c.should.exactly(a);
+
+      c = utils.merge(a, undefined);
+      c.should.exactly(a);
+
+      c = utils.merge(a, '');
+      c.should.exactly(a);
+
+      c = utils.merge(a, 5);
+      c.should.exactly(a);
+
+    });
+    
+  });
+
+  describe('filter unit test', function () {
+
+    it('baisc test', function () {
+      var a = {
+        k1: 'a1',
+        k2: 'a2',
+        k3: 'a3'
+      };
+      var b = utils.filter(a, ['k1', 'k2', 'k3']);
+      
+      b.should.not.exactly(a);
+      b.should.eql(a);
+
+      b = utils.filter(a, 'k1, k2');
+      b.should.have.property('k1');
+      b.should.have.property('k2');
+      b.should.not.have.property('k3');
+
+    });
+  });
+
+});
